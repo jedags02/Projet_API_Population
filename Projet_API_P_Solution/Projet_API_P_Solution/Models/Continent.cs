eca93cb8eb3d845aa2f1de3d17beece7a09fbf5d
Hanni24 <alabihanane@yahoo.com>
2022-03-01 17:17:34 +0100

    ajout_1_h

---
+++ Projet_API_P_Solution/Projet_API_P_Solution/Models/Continent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projet_API_P_Solution.Models
+{
+    public class Continent
+    {
+        public int id { get; set; }
+        public string nom { get; set; }
+
+        public List<Pays>LesPays { get; set; }
+
+    }
+}
