6b52b6738943c370ece328ceb73ea6ad0a3afdb3
Jédielle HOUNSONLON <detondji.hounsonlon@groupe-esigelec.org>
2022-03-01 17:05:45 +0100

    Ajout_1_J

---
+++ Projet_API_P_Solution/Projet_API_P_Solution/Models/Pays.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projet_API_P_Solution.Models
+{
+    public class Pays
+    {
+    }
+}
