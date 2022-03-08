3d57f27f85b7afdd74ea4c45e29ad354de3269dc
Hanni24 <alabihanane@yahoo.com>
2022-03-08 14:16:05 +0100

    ajout_h

---
+++ Projet_API_P_Solution/Projet_API_P_Solution/Controllers/ContinentsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Projet_API_P_Solution.Data;
+using Projet_API_P_Solution.Models;
+
+namespace Projet_API_P_Solution.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContinentsController : ControllerBase
+    {
+        private readonly Projet_API_P_SolutionContext _context;
+
+        public ContinentsController(Projet_API_P_SolutionContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Continents
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Continent>>> GetContinent()
+        {
+            return await _context.Continent.ToListAsync();
+        }
+
+        // GET: api/Continents/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Continent>> GetContinent(int id)
+        {
+            var continent = await _context.Continent.FindAsync(id);
+
+            if (continent == null)
+            {
+                return NotFound();
+            }
+
+            return continent;
+        }
+
+        // PUT: api/Continents/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutContinent(int id, Continent continent)
+        {
+            if (id != continent.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(continent).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContinentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Continents
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Continent>> PostContinent(Continent continent)
+        {
+            _context.Continent.Add(continent);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetContinent", new { id = continent.id }, continent);
+        }
+
+        // DELETE: api/Continents/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Continent>> DeleteContinent(int id)
+        {
+            var continent = await _context.Continent.FindAsync(id);
+            if (continent == null)
+            {
+                return NotFound();
+            }
+
+            _context.Continent.Remove(continent);
+            await _context.SaveChangesAsync();
+
+            return continent;
+        }
+
+        private bool ContinentExists(int id)
+        {
+            return _context.Continent.Any(e => e.id == id);
+        }
+    }
+}
