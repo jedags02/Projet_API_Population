c4e5caf3c2351cc11963586aa6a0c3b9a9c737a2
Jédielle HOUNSONLON <detondji.hounsonlon@groupe-esigelec.org>
2022-03-08 14:14:42 +0100

    Ajout_3_J Controlleurs Pays

---
+++ Projet_API_P_Solution/Projet_API_P_Solution/Controllers/PaysController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Projet_API_P_Solution.Data;
+using Projet_API_P_Solution.Models;
+
+namespace Projet_API_P_Solution.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaysController : ControllerBase
+    {
+        private readonly Projet_API_P_SolutionContext _context;
+
+        public PaysController(Projet_API_P_SolutionContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Pays
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Pays>>> GetPays()
+        {
+            return await _context.Pays.ToListAsync();
+        }
+
+        // GET: api/Pays/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pays>> GetPays(int id)
+        {
+            var pays = await _context.Pays.FindAsync(id);
+
+            if (pays == null)
+            {
+                return NotFound();
+            }
+
+            return pays;
+        }
+
+        // PUT: api/Pays/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPays(int id, Pays pays)
+        {
+            if (id != pays.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pays).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaysExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Pays
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Pays>> PostPays(Pays pays)
+        {
+            _context.Pays.Add(pays);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPays", new { id = pays.id }, pays);
+        }
+
+        // DELETE: api/Pays/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Pays>> DeletePays(int id)
+        {
+            var pays = await _context.Pays.FindAsync(id);
+            if (pays == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pays.Remove(pays);
+            await _context.SaveChangesAsync();
+
+            return pays;
+        }
+
+        private bool PaysExists(int id)
+        {
+            return _context.Pays.Any(e => e.id == id);
+        }
+    }
+}
--- Projet_API_P_Solution/Projet_API_P_Solution/Models/Pays.cs
+++ Projet_API_P_Solution/Projet_API_P_Solution/Models/Pays.cs
@@ -10,8 +10,8 @@ namespace Projet_API_P_Solution.Models
        
             public int id { get; set; }
             public String nomPays { get; set; }
-            
-             
-        
+            public Continent continent { get; set; }
+            public List<Population> LaPopulation { get; set; }
+
     }
 }
