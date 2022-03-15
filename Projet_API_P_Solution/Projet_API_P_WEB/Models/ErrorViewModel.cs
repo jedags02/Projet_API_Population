37a351eb1450476cdcb6611f4a3d25053be79477
Jédielle HOUNSONLON <detondji.hounsonlon@groupe-esigelec.org>
2022-03-15 17:12:32 +0100

    Ajout_J_Création api web

---
+++ Projet_API_P_Solution/Projet_API_P_WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Projet_API_P_WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
